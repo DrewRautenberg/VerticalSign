dfc5510ffed70993386345d259012a9eedb566b2
Drew Rautenberg <drew@drewrautenberg.com>
2017-10-04 15:44:54 -0400

    print left right vertical method

--- src/verticalsign/Draw.java
+++ src/verticalsign/Draw.java
@@ -10,20 +10,31 @@ package verticalsign;
         }
     }
     //printMixedHor
-    static void printLVer(int height){
-         for (int i=0; i<height;i++){
+    static void printLVer(int ht){
+         for (int i=0; i<ht;i++){
              System.out.println("XX");
          }
     }
-    static void printRVer(int width, int height){
-        int distRight = width-2;
-        for (int i=0; i<height;i++) {
+    static void printRVer(int wid, int ht){
+        int distRight = wid-2;
+        for (int i=0; i<ht;i++) {
 
             for (int j = 0; j < distRight; j++) {
                 System.out.print(" ");
             }
             System.out.println("XX");
         }
+    }
+    static void printLRVer(int wid, int ht){
+        int dis = wid-4;
+        System.out.print("XX");
+        for (int i=0; i<ht; i++) {
+            for (int j = 0; j < dis; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("XX");
+        }
+
     }
 
 }
