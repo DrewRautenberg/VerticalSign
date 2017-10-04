3aa5ce6ea12dfe7691889a64518423ea1a1f9032
Drew Rautenberg <drew@drewrautenberg.com>
2017-10-04 15:57:56 -0400

    print mixed horizontal

--- src/verticalsign/Draw.java
+++ src/verticalsign/Draw.java
@@ -10,7 +10,19 @@ class Draw {
         }
     }
 
-    //printMixedHor
+    static void printMixedHor(int wid, int numSpaces){
+        int printWid = wid - numSpaces;
+        for (int i=0;i<2;i++){
+            for (int j=0;j<numSpaces;j++){
+                System.out.print(" ");
+            }
+            for (int k=0; k<printWid;k++){
+                System.out.print("X");
+            }
+            System.out.println();
+        }
+
+    }
     static void printLVer(int ht) {
         for (int i = 0; i < ht; i++) {
             System.out.println("XX");
