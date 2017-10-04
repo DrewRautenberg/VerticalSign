602b53ed22b4083979f6ca82b0cca68c23ba8613
Drew Rautenberg <drew@drewrautenberg.com>
2017-10-04 15:47:45 -0400

    fixed print left right vertical method

--- src/verticalsign/Draw.java
+++ src/verticalsign/Draw.java
@@ -1,23 +1,25 @@
 package verticalsign;
 
- class Draw {
-     static void printHor(int wid){
-        for (int i=0; i<2;i++){
-            for (int j=0; j<wid;j++){
+class Draw {
+    static void printHor(int wid) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < wid; j++) {
                 System.out.print("X");
             }
             System.out.println();
         }
     }
+
     //printMixedHor
-    static void printLVer(int ht){
-         for (int i=0; i<ht;i++){
-             System.out.println("XX");
-         }
+    static void printLVer(int ht) {
+        for (int i = 0; i < ht; i++) {
+            System.out.println("XX");
+        }
     }
-    static void printRVer(int wid, int ht){
-        int distRight = wid-2;
-        for (int i=0; i<ht;i++) {
+
+    static void printRVer(int wid, int ht) {
+        int distRight = wid - 2;
+        for (int i = 0; i < ht; i++) {
 
             for (int j = 0; j < distRight; j++) {
                 System.out.print(" ");
@@ -25,10 +27,11 @@ package verticalsign;
             System.out.println("XX");
         }
     }
-    static void printLRVer(int wid, int ht){
-        int dis = wid-4;
-        System.out.print("XX");
-        for (int i=0; i<ht; i++) {
+
+    static void printLRVer(int wid, int ht) {
+        int dis = wid - 4;
+        for (int i = 0; i < ht; i++) {
+            System.out.print("XX");
             for (int j = 0; j < dis; j++) {
                 System.out.print(" ");
             }
--- src/verticalsign/VerticalSign.java
+++ src/verticalsign/VerticalSign.java
@@ -2,5 +2,6 @@ package verticalsign;
 
 public class VerticalSign {
     public static void main(String[] args) {
+        Draw.printLRVer(10,15);
     }
 }
