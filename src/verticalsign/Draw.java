c94c204ee3bbe393fc26e8c3fe0e548d1b5c8293
Drew Rautenberg <drew@drewrautenberg.com>
2017-10-04 15:36:01 -0400

    print left vertical method

--- src/verticalsign/Draw.java
+++ src/verticalsign/Draw.java
@@ -1,13 +1,19 @@
 package verticalsign;
 
-public class Draw {
-    public static void printHor(int wid){
+ class Draw {
+     static void printHor(int wid){
         for (int i=0; i<2;i++){
             for (int j=0; j<wid;j++){
                 System.out.print("X");
             }
             System.out.println();
         }
+    }
+    //printMixedHor
+    static void printLVer(int height){
+         for (int i=0; i<height;i++){
+             System.out.println("XX");
+         }
     }
 
 }
--- src/verticalsign/VerticalSign.java
+++ src/verticalsign/VerticalSign.java
@@ -2,6 +2,6 @@ package verticalsign;
 
 public class VerticalSign {
     public static void main(String[] args) {
-
+        Draw.printLVer(5);
     }
 }
