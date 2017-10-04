8b6bf05bf8a5499b6b0f881e7d2e86a78fddd367
Drew Rautenberg <drew@drewrautenberg.com>
2017-10-04 15:33:40 -0400

    print horizontal method

--- src/verticalsign/Draw.java
+++ src/verticalsign/Draw.java
@@ -1,5 +1,13 @@
 package verticalsign;
 
 public class Draw {
+    public static void printHor(int wid){
+        for (int i=0; i<2;i++){
+            for (int j=0; j<wid;j++){
+                System.out.print("X");
+            }
+            System.out.println();
+        }
+    }
 
 }
