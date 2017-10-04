06afb857c40cef965ea13097ff7d5e1301d56b9b
Drew Rautenberg <drew@drewrautenberg.com>
2017-10-04 15:41:45 -0400

    print right vertical method

--- src/verticalsign/Draw.java
+++ src/verticalsign/Draw.java
@@ -15,5 +15,15 @@ package verticalsign;
              System.out.println("XX");
          }
     }
+    static void printRVer(int width, int height){
+        int distRight = width-2;
+        for (int i=0; i<height;i++) {
+
+            for (int j = 0; j < distRight; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("XX");
+        }
+    }
 
 }
--- src/verticalsign/VerticalSign.java
+++ src/verticalsign/VerticalSign.java
@@ -2,6 +2,5 @@ package verticalsign;
 
 public class VerticalSign {
     public static void main(String[] args) {
-        Draw.printLVer(5);
     }
 }
