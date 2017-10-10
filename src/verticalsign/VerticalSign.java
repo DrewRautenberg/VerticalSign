dabe7a9ecd078957f6f5eb84f3d6b07e9c480841
Drew Rautenberg <drew@drewrautenberg.com>
2017-10-10 12:44:30 -0400

    get user input

--- src/verticalsign/VerticalSign.java
+++ src/verticalsign/VerticalSign.java
@@ -1,8 +1,18 @@
 package verticalsign;
 
+import javax.swing.*;
+
 public class VerticalSign {
     public static void main(String[] args) {
         int ht=11;
         int wid=10;
+        String userInput;
+        String numIn;
+        JOptionPane.showInputDialog("Please enter phrase to display");
+        numIn=JOptionPane.showInputDialog("Height:");
+        ht=Integer.parseInt(numIn);
+        numIn=JOptionPane.showInputDialog("Width:");
+        wid=Integer.parseInt(numIn);
+
     }
 }
